4ef68e80f66ebce793a9aea7e927ab29901b85b3
massielgomez-ops <massiel.gomez@vallegrande.edu.pe>
2026-06-24 01:20:19 -0500

    feat(backend): add deleted endpoints for categories and crops

--- backend/src/main/java/vallegrande/edu/pe/backend/rest/CategoriaController.java
+++ backend/src/main/java/vallegrande/edu/pe/backend/rest/CategoriaController.java
@@ -1,8 +1,12 @@
 package vallegrande.edu.pe.backend.rest;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import vallegrande.edu.pe.backend.model.Categoria;
@@ -19,5 +23,11 @@ public class CategoriaController extends AbstractCrudController<Categoria> {
 	@Override
 	protected AbstractCrudService<Categoria> service() {
 		return service;
+	}
+
+	@GetMapping("/eliminados")
+	@Operation(summary = "Lista todas las categorías eliminadas lógicamente")
+	public List<Categoria> findAllDeleted() {
+		return service.findAllDeleted();
 	}
 }
--- backend/src/main/java/vallegrande/edu/pe/backend/rest/CultivoController.java
+++ backend/src/main/java/vallegrande/edu/pe/backend/rest/CultivoController.java
@@ -1,8 +1,12 @@
 package vallegrande.edu.pe.backend.rest;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import vallegrande.edu.pe.backend.model.Cultivo;
@@ -19,5 +23,11 @@ public class CultivoController extends AbstractCrudController<Cultivo> {
 	@Override
 	protected AbstractCrudService<Cultivo> service() {
 		return service;
+	}
+
+	@GetMapping("/eliminados")
+	@Operation(summary = "Lista todos los cultivos eliminados lógicamente")
+	public List<Cultivo> findAllDeleted() {
+		return service.findAllDeleted();
 	}
 }
